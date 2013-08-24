9871570537690bfdb5e9c8df6abc0646ed8d1a3e
Valentin LEMIERE <me@ibilon.com>
2013-08-24 10:24:09 +0200

    HaxePunk template.

---
+++ src/Main.hx
@@ -0,0 +1,16 @@
+import com.haxepunk.Engine;
+import com.haxepunk.HXP;
+
+class Main extends Engine
+{
+	override public function init()
+	{
+		#if debug HXP.console.enable(); #end
+		// HXP.scene = new YourScene();
+	}
+
+	public static function main() 
+	{
+		new Main();
+	}
+}
